ceb538c26fabb03fd191e1aa9c79a3c5cea53104
a11947039 <a11947039@gmail.com>
2016-09-07 17:33:51 +0800

    初版account記錄

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accdate = new System.Windows.Forms.DateTimePicker();
+            this.acctype = new System.Windows.Forms.ComboBox();
+            this.accmoney = new System.Windows.Forms.TextBox();
+            this.accsp = new System.Windows.Forms.TextBox();
+            this.accsave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.accpath = new System.Windows.Forms.TextBox();
+            this.accopen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // accdate
+            // 
+            this.accdate.Location = new System.Drawing.Point(12, 53);
+            this.accdate.Name = "accdate";
+            this.accdate.Size = new System.Drawing.Size(200, 22);
+            this.accdate.TabIndex = 0;
+            // 
+            // acctype
+            // 
+            this.acctype.FormattingEnabled = true;
+            this.acctype.Items.AddRange(new object[] {
+            "早餐",
+            "午餐",
+            "晚餐",
+            "飲料",
+            "油費"});
+            this.acctype.Location = new System.Drawing.Point(12, 81);
+            this.acctype.Name = "acctype";
+            this.acctype.Size = new System.Drawing.Size(121, 20);
+            this.acctype.TabIndex = 1;
+            // 
+            // accmoney
+            // 
+            this.accmoney.Location = new System.Drawing.Point(13, 108);
+            this.accmoney.Name = "accmoney";
+            this.accmoney.Size = new System.Drawing.Size(100, 22);
+            this.accmoney.TabIndex = 2;
+            // 
+            // accsp
+            // 
+            this.accsp.Location = new System.Drawing.Point(13, 137);
+            this.accsp.Name = "accsp";
+            this.accsp.Size = new System.Drawing.Size(100, 22);
+            this.accsp.TabIndex = 3;
+            // 
+            // accsave
+            // 
+            this.accsave.Location = new System.Drawing.Point(13, 166);
+            this.accsave.Name = "accsave";
+            this.accsave.Size = new System.Drawing.Size(75, 23);
+            this.accsave.TabIndex = 4;
+            this.accsave.Text = "save";
+            this.accsave.UseVisualStyleBackColor = true;
+            this.accsave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(330, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "EXCEL路徑";
+            // 
+            // accpath
+            // 
+            this.accpath.Location = new System.Drawing.Point(85, 13);
+            this.accpath.Name = "accpath";
+            this.accpath.Size = new System.Drawing.Size(127, 22);
+            this.accpath.TabIndex = 8;
+            this.accpath.Text = "E:\\\\accounttemp.xlsx";
+            // 
+            // accopen
+            // 
+            this.accopen.Location = new System.Drawing.Point(218, 13);
+            this.accopen.Name = "accopen";
+            this.accopen.Size = new System.Drawing.Size(75, 23);
+            this.accopen.TabIndex = 9;
+            this.accopen.Text = "OPEN";
+            this.accopen.UseVisualStyleBackColor = true;
+            this.accopen.Click += new System.EventHandler(this.accopen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 508);
+            this.Controls.Add(this.accopen);
+            this.Controls.Add(this.accpath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.accsave);
+            this.Controls.Add(this.accsp);
+            this.Controls.Add(this.accmoney);
+            this.Controls.Add(this.acctype);
+            this.Controls.Add(this.accdate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker accdate;
+        private System.Windows.Forms.ComboBox acctype;
+        private System.Windows.Forms.TextBox accmoney;
+        private System.Windows.Forms.TextBox accsp;
+        private System.Windows.Forms.Button accsave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox accpath;
+        private System.Windows.Forms.Button accopen;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApplication2
+{
+    
+    public partial class Form1 : Form
+    {
+        Excel.Application _Excel = null;
+        int nowrow = 1;
+        Excel.Workbook book = null;
+        Excel.Worksheet sheet = null;
+        Excel.Range range = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = accdate.Text;
+            label1.Text += acctype.Text;
+            label1.Text += accmoney.Text;
+            label1.Text += accsp.Text;
+            range = sheet.get_Range("A" + nowrow.ToString());
+            range.set_Value(Type.Missing, accdate.Text);
+            range = sheet.get_Range("B" + nowrow.ToString());
+            range.set_Value(Type.Missing, acctype.Text);
+            range = sheet.get_Range("C" + nowrow.ToString());
+            range.set_Value(Type.Missing, accmoney.Text);
+            range = sheet.get_Range("D" + nowrow.ToString());
+            range.set_Value(Type.Missing, accsp.Text);
+            nowrow += 1;
+            this.accdate.Focus();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //全選
+            accmoney.GotFocus += (object sender1, EventArgs e1) =>
+                {
+                    accmoney.SelectAll();
+                };
+            accsp.GotFocus += (object sender2, EventArgs e2) =>
+            {
+                accsp.SelectAll();
+            };
+
+            
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            sheet.Application.DisplayAlerts = false;
+            sheet.Application.AlertBeforeOverwriting = false;
+            book.Save();
+
+            book.Close();
+            this._Excel.Quit();
+            this._Excel = null;
+            //確認已經沒有excel工作再回收
+            GC.Collect();
+            label2.Text = "CLOSE";
+        }
+
+        private void accopen_Click(object sender, EventArgs e)
+        {
+            bool flag = false;
+            foreach (var item in Process.GetProcesses())
+            {
+                if (item.ProcessName == "EXCEL")
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+            {
+                this._Excel = new Excel.Application();
+            }
+            else
+            {
+                object obj = Marshal.GetActiveObject("Excel.Application");//引用已在執行的Excel
+                _Excel = obj as Excel.Application;
+            }
+
+            this._Excel.Visible = true;//設false效能會比較好
+
+
+            string path = "E:\\accounttemp.xlsx";
+
+            book = _Excel.Workbooks.Open(path, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);//開啟舊檔案
+            sheet = (Excel.Worksheet)book.Sheets[1];
+
+            range = sheet.get_Range("A" + nowrow.ToString());
+            string temptext = range.Cells.Text;
+            while (temptext != "")
+            {
+                nowrow += 1;
+                label1.Text += range.Cells.Text;
+                range = sheet.get_Range("A" + nowrow.ToString());
+                temptext = range.Cells.Text;
+            }
+
+
+
+            //foreach (Excel.Range item in range)
+            //{
+            //    //Console.WriteLine(item.Cells.Formula);
+            //    //Console.WriteLine(item.Cells.Value2);
+            //    //Console.WriteLine(item.Cells.Text);
+            //    label1.Text += item.Cells.Formula;
+            //    label1.Text += item.Cells.Value2;
+            //    label1.Text += item.Cells.Text;
+            //}
+
+            label2.Text = "OPENING";
+        }
+    }
+}
